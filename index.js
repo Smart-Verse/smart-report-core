ede3a5410911df60a41df844e9d08ce3f27381a9
Geovane-Araujo <geovane-araujo@outlook.com>
2024-12-24 15:20:36 -0300

    sadsd

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const { MongoClient, ObjectId} = require('mongodb');
-const Mustache = require("mustache");
 const app = express();
 const PORT = 5071;
 
 app.use(express.json());
 
-const uri = "mongodb://root:553322@localhost:27017";
-const client = new MongoClient(uri);
-
 
 app.post('/report', async (req, res)  => {
     try{
-        var report = await generateReport(req.body);
-
+        var report = await generateReport(req.body.report);
         res.set({
             'Content-Type': 'application/pdf',
             'Content-Disposition': 'inline; filename="generated.pdf"',
@@ -33,36 +27,24 @@ app.listen(PORT, () => {
 });
 
 
-async function generateReport(data){
+async function generateReport(template){
 
-
-    const templateHtml = await connectAndFetch();
-
-    const finalHtml = Mustache.render(templateHtml,  { data: data.data });
     const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+
     const page = await browser.newPage();
-
-    await page.setContent(finalHtml);
-
-    const pdfBuffer = await page.pdf({ format: 'A4' });
+    await page.setContent(template);
+    const pdfBuffer = await page.pdf(
+{
+            format: 'A4' ,
+            displayHeaderFooter: false,
+            background: true
+        });
     await browser.close();
 
-    return pdfBuffer;
+    const pdf = Buffer.from(pdfBuffer);
 
-}
+    return pdf;
 
-async function connectAndFetch() {
-    try {
-        await client.connect();
-        const db = client.db("db_report");
-        const collection = db.collection("report");
-        const document = await collection.findOne({ _id: new ObjectId("6747cbe9ed10506c48e28f6d") });
-        return document.model;
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-    } finally {
-        await client.close();
-    }
 }

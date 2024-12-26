bc765bc5e8b08f045c5701c28c0520eeb7c565bf
Geovane Araujo <geovane-araujo@outlook.com>
2024-12-26 20:50:21 -0300

    ajustes

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ async function generateReport(template){
         const browser = await puppeteer.launch({
             executablePath: '/usr/bin/google-chrome-stable',
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage'],
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--no-zygote',
+                '--single-process'
+            ],
             timeout: 60000
         });
     
